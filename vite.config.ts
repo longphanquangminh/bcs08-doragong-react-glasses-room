86002d1cc12af7fd45e2a907bf47223c55a805d8
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-08-31 19:41:44 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Base URL for your application
+  base: "/",
+
+  // Build output directory
+  build: {
+    outDir: "dist",
+  },
+
+  // Configure server settings
+  // server: {
+  //   port: 3000,
+  // },
+
+  // Additional resolve configuration
+  resolve: {
+    alias: {
+      // Add your own aliases here if needed
+    },
+  },
+});
